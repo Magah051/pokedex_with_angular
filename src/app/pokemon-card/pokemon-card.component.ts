b474589ae5a7591c6b58f5e146bea273a0b3a7ce
José Magalhães <joses.magalhaes@hotmail.com>
2023-11-24 19:58:15 -0300

    Construção da base da página

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.sass']
+})
+export class PokemonCardComponent {
+
+
+}
